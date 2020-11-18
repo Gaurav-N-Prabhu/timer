d7192cef269d9ea975be76c523f8e9ee375f8dba
prabhu-gaurav <54278211+prabhu-gaurav@users.noreply.github.com>
2020-11-18 12:23:15 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -9,7 +9,7 @@ setInterval( function()
         id = document.getElementById("count").innerHTML = counter;
 		id = document.getElementById("sec").innerHTML = "sec";
     }
-	if(counter == 0)
+	if(counter == -1)
 	{
 		id = document.getElementById("count").innerHTML = "COMPLETE";
 		id = document.getElementById("sec").innerHTML = "";
